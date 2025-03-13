b5e39b4d80b34fda93a1670a6a9754e8f68a4c0a
Shashank0406 <shashankkurakula.417@gmail.com>
2025-03-13 11:13:58 -0400

    Project files and packages ready with env

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shashank Kurakula",
+    author_email="shashankkurakula5@gmail.com",
+    packages=find_packages()
+)
